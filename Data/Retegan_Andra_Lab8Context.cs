a1b4a0334b7adf5ee77977d39fc7b0244e23bbf8
ANDRA RETEGAN <retegan.andra174061@student.ubbcluj.ro>
2022-01-15 20:57:05 +0200

    Add project files.

---
+++ Data/Retegan_Andra_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Retegan_Andra_Lab8.Models;
+
+namespace Retegan_Andra_Lab8.Data
+{
+    public class Retegan_Andra_Lab8Context : DbContext
+    {
+        public Retegan_Andra_Lab8Context (DbContextOptions<Retegan_Andra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Retegan_Andra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Retegan_Andra_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Retegan_Andra_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Retegan_Andra_Lab8.Data;
+using Retegan_Andra_Lab8.Models;
+
+namespace Retegan_Andra_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Retegan_Andra_Lab8.Data.Retegan_Andra_Lab8Context _context;
+
+        public IndexModel(Retegan_Andra_Lab8.Data.Retegan_Andra_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
